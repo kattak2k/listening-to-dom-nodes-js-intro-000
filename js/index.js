74fe3da3043d62633de85669a270fc31bf6a5a6b
Ravi <kkiran.ror@gmail.com>
2020-03-15 03:41:53 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -44,11 +44,11 @@ for (let i = 0; i < divs.length; i++) {
 //capturing
 
 divs = document.querySelectorAll('div');
- 
+
 function capture(e) {
   console.log(this.firstChild.nodeValue.trim() + ' captured');
 }
- 
+
 for (let i = 0; i < divs.length; i++) {
   // set the third argument to `true`!
   divs[i].addEventListener('click', capture, true);
