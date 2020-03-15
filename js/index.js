8303c092398ff8f49f71e35c53a06bf9909e42f2
Ravi <kkiran.ror@gmail.com>
2020-03-15 03:46:16 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -27,13 +27,6 @@ input.addEventListener('keydown', function(e) {
 let divs = document.querySelectorAll('div');
 
 function bubble(e) {
-  // remember all of those fancy DOM node properties?
-  // we're making use of them to get the number
-  // in each div here!
-
-  // if `this` is a bit confusing, don't worry —
-  // for now, know that it refers to the div that
-  // is triggering the current event handler.
   console.log(this.firstChild.nodeValue.trim() + ' bubbled');
 }
 
@@ -42,7 +35,6 @@ for (let i = 0; i < divs.length; i++) {
 }
 
 //capturing
-
 divs = document.querySelectorAll('div');
 
 function capture(e) {
